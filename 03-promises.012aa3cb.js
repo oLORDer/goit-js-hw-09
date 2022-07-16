21450b29ee803be5df46bd305ebe59a954e250c2
oLORDer <70772601+oLORDer@users.noreply.github.com>
2022-07-16 12:54:18 +0000

    Deploying to gh-pages from @ oLORDer/goit-js-hw-09@816edd16257b999611ded3e705d498bd7f3645aa 🚀

--- 03-promises.012aa3cb.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".form").addEventListener("submit",(function(e,t){Math.random()}));
-//# sourceMappingURL=03-promises.012aa3cb.js.map
--- 03-promises.e66a237d.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".form").addEventListener("submit",(function(e,t){Math.random()}));
-//# sourceMappingURL=03-promises.e66a237d.js.map
