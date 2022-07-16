573a54d2488f0b9a70fa1a1d144892baf58e6962
Andrii <andryushka137@gmail.com>
2022-07-16 15:08:44 +0300

    ninaiu promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,5 @@
+const form = document.querySelector('.form');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +8,5 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+form.addEventListener('submit', createPromise);
